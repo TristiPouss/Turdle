575faad242d2d88e733fe26e977b6ee7c080640b
TristiPouss <tristandsg@gmail.com>
2024-11-08 16:20:20 +0100

    Added base game keyboard, grid for guesses etc

--- turdle/lib/TurdleMainMenu.dart
+++ turdle/lib/TurdleMainMenu.dart
@@ -1,121 +1,144 @@
 import 'package:flutter/material.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TurdleMainMenu extends StatelessWidget {
+  const TurdleMainMenu({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Turdle',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TurdleGamePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class TurdleGamePage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _TurdleGamePageState createState() => _TurdleGamePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TurdleGamePageState extends State<TurdleGamePage> {
+  // Exemple de mot cible
+  final String targetWord = "DARTS";
+  // Liste pour stocker les mots devinés
+  List<String> guesses = ["", "", "", "", "", ""];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  // Lettres tapées par l'utilisateur
+  int currentGuess = 0;
+
+  void onLetterTap(String letter) {
+    if (guesses[currentGuess].length < targetWord.length) {
+      setState(() {
+        guesses[currentGuess] += letter;
+      });
+    }
+  }
+
+  void onBackspaceTap() {
+    if (guesses[currentGuess].isNotEmpty) {
+      setState(() {
+        guesses[currentGuess] = guesses[currentGuess].substring(0, guesses[currentGuess].length - 1);
+      });
+    }
+  }
+
+  void onEnterTap() {
+    if (guesses[currentGuess].length == targetWord.length) {
+      setState(() {
+        checkGuess();
+        currentGuess++;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Turdle'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: [
+          // Grille des mots devinés
+          Expanded(
+            child: GridView.builder(
+              itemCount: guesses.length * targetWord.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: targetWord.length,
+              ),
+              itemBuilder: (context, index) {
+                int wordIndex = index ~/ targetWord.length;
+                int letterIndex = index % targetWord.length;
+                String letter = guesses[wordIndex].length > letterIndex
+                    ? guesses[wordIndex][letterIndex]
+                    : "";
+
+                return Container(
+                  margin: EdgeInsets.all(4.0),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    color: Colors.white,
+                  ),
+                  child: Text(
+                    letter,
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                );
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          ),
+          // Clavier virtuel
+          buildKeyboard(),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  Widget buildKeyboard() {
+    final letters = [
+      'A', 'Z', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
+      'Q', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M',
+      'W', 'X', 'C', 'V', 'B', 'N'
+    ];
+
+    return Column(
+      children: [
+        Wrap(
+          alignment: WrapAlignment.center,
+          spacing: 8.0,
+          runSpacing: 8.0,
+          children: letters.map((letter) {
+            return ElevatedButton(
+              onPressed: () => onLetterTap(letter),
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(36, 36),
+                padding: EdgeInsets.all(12.0),
+              ),
+              child: Text(letter),
+            );
+          }).toList(),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              onPressed: onBackspaceTap,
+              icon: Icon(Icons.backspace),
+            ),
+            SizedBox(width: 8),
+            ElevatedButton(
+              onPressed: onEnterTap,
+              child: Text('Enter'),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  void checkGuess() {}
 }
--- turdle/lib/main.dart
+++ turdle/lib/main.dart
@@ -2,5 +2,5 @@ import 'TurdleMainMenu.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const TurdleMainMenu());
 }
